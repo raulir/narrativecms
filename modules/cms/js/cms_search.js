6ef64a0470afd68e0f1f342428facc17f12c13b2
Rauli Rajande <rauli@bytecrackers.com>
2018-01-27 14:43:07 +0000

    #217 cms search beautification

--- modules/cms/js/cms_search.js
+++ modules/cms/js/cms_search.js
@@ -25,7 +25,7 @@ function cms_search_init(){
 					
 					// list item pages
 					if (data.result.result.pages.lists){
-						$('.cms_search_result_pages').append('<div class="cms_column_header">List pages</div>');
+						$('.cms_search_result_pages').append('<div class="cms_column_header">List pages and partials</div>');
 						$.each(data.result.result.pages.lists, function(key, value){
 							$('.cms_search_result_pages').append('<div class="cms_search_item cms_search_' + value.show + '"><div class="cms_search_title">' + value.title + '</div><a class="cms_search_edit" href="' + config_url + 'admin/' + value.edit_url + '">edit</a>' + 
 									(value.slug || value.page_id == 1 ? '<a target="_blank" class="cms_search_view" href="' + config_url + (value.slug != '' ? (value.slug + '/') : '') + '">view</a>' : '') + '</div>');
@@ -57,7 +57,8 @@ function cms_search_init(){
 						$('.cms_search_result_panels').append('<div class="cms_column_header">Other panels</div>');
 						$.each(data.result.result.page_panels.settings, function(key, value){
 							$('.cms_search_result_panels').append('<div class="cms_search_item cms_search_' + value.show + '"><div class="cms_search_title">' + value.title + '</div>' +
-									'<a class="cms_search_edit" href="' + config_url + 'admin/' + value.edit_url + '">edit</a></div>');
+									( value.edit_url ? '<a class="cms_search_edit" href="' + config_url + 'admin/' + value.edit_url + '">edit</a>' : '') + 
+									'</div>');
 						});
 					}
 					
