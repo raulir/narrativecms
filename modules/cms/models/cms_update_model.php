d17db76a5b9755122e6a3f1377658c15fcda506d
Rauli Rajande <rauli@bytecrackers.com>
2018-03-29 16:49:35 +0100

    #242 updated license

--- modules/cms/models/cms_update_model.php
+++ modules/cms/models/cms_update_model.php
@@ -81,12 +81,12 @@ class cms_update_model extends CI_Model {
 		$version_hashes[] = $cms_md5;
 		
 		// LICENCE
-		if (file_exists(str_replace("\\", '/', $GLOBALS['config']['base_path']).'LICENCE')){
-			$cms_md5 = md5_file(str_replace("\\", '/', $GLOBALS['config']['base_path']).'LICENCE');
+		if (file_exists(str_replace("\\", '/', $GLOBALS['config']['base_path']).'LICENSE')){
+			$cms_md5 = md5_file(str_replace("\\", '/', $GLOBALS['config']['base_path']).'LICENSE');
 			$hashes[] = array(
 					'filename' => 'LICENCE',
 					'hash' => $cms_md5,
-					'size' => filesize(str_replace("\\", '/', $GLOBALS['config']['base_path']).'LICENCE'),
+					'size' => filesize(str_replace("\\", '/', $GLOBALS['config']['base_path']).'LICENSE'),
 			);
 			$version_hashes[] = $cms_md5;
 		}
