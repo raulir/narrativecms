ef6e10c324b821dc907cd2b46e2413aad5b0d9d2
Rauli Rajande <rauli@bytecrackers.com>
2019-03-26 11:24:25 +0000

    #356 list item ajax panel automatical population

--- application/controllers/ajax_api.php
+++ application/controllers/ajax_api.php
@@ -11,14 +11,36 @@ class Ajax_api extends CI_Controller {
    	function get_panel(){
    		if (empty($this->params['panel_id'])){
     		print(json_encode(array('result' => array(), 'error' => array('message' => 'Missing panel_id', ))));
-    		exit();
+    		die();
    		}
-   		
-    	if (extension_loaded('newrelic')) {
+
+   		if (extension_loaded('newrelic')) {
     		
 			newrelic_set_appname($GLOBALS['config']['title']);
 			newrelic_name_transaction('/ajax_api/'.$this->params['panel_id']);
 
+		}
+		
+		// if list item, load data
+		if (!empty($this->params['cms_page_panel_id']) && empty($this->params['no_html'])){
+			
+			$this->load->model('cms/cms_panel_model');
+			$this->load->model('cms/cms_page_panel_model');
+			
+			$panel_config = $this->cms_panel_model->get_cms_panel_config($this->params['panel_id']);
+			
+			if (!empty($panel_config['list']['ajax_target'])){
+				$this->params = array_merge($this->cms_page_panel_model->get_cms_page_panel($this->params['cms_page_panel_id']), $this->params);
+			} else {
+				print(json_encode(array('result' => array(), 'error' => array('message' => 'Not list', ))));
+				die();
+			}
+			
+			if (empty($this->params['show'])){
+				print(json_encode(array('result' => array(), 'error' => array('message' => 'Can\'t show', ))));
+				die();
+			}
+			
 		}
    		
 		$data = $this->ajax_panel($this->params['panel_id'], $this->params);
