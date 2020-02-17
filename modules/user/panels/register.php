225ed8a65b5fa60b5a41175fe5059ed09c7b11ed
raulir <rauli@bytecrackers.com>
2020-02-17 15:48:42 +0000

    #user module registration

--- modules/user/panels/register.php
+++ modules/user/panels/register.php
@@ -2,7 +2,58 @@
 
 class register extends CI_Controller{
 	
+	function panel_action($params){
+		
+		$this->load->model('user/user_model');
+		
+		$do = $this->input->post('do');
+
+		if ($do == 'register'){
+			 
+			$email = $this->input->post('email');
+			$first_name = $this->input->post('first_name');
+			$last_name = $this->input->post('last_name');
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+			$password2 = $this->input->post('password2');
+			
+			if ($password !== $password2){
+				$error = 'password_mismatch';
+			}
+			
+			if (empty($error)){
+				
+				$user = $this->user_model->create_user([
+						'username' => $username,
+						'email' => $email,
+						'first_name' => $first_name,
+						'last_name' => $last_name,
+						'password' => $password,
+				]);
+				
+				if (!empty($user['error'])){
+					$error = $user['error'];
+				}
+				
+			}
+			
+		}
+		
+		if (!empty($error)){
+			$params['error'] = $error;
+		}
+
+		return $params;
+		
+	}
+	
 	function panel_params($params){
+		
+		
+		print('<pre>');
+		print_r($params);
+		print('</pre>');
+		
 
 		// check if logged in
 		$params['loggedin'] = 0;
--- modules/user/panels/user.php
+++ modules/user/panels/user.php
@@ -1,10 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class user extends MY_Controller{
+class user extends CI_Controller{
 	
 	function panel_heading($params){
+		
+		if (!empty($params['username'])){
+			$first_part = $params['username'];
+			$last_part = ' &lt;' . $params['email'] . '&gt;';
+		} else {
+			$first_part = $params['email'];
+			$last_part = '';
+		}
 
-		return $params['username'] . ' (' . $params['fullname'] . ') &lt;' . $params['email'] . '&gt;';
+		return $first_part . ' (' . $params['first_name'] . ' ' . $params['last_name'] . ')' . $last_part;
 	
 	}
 	
