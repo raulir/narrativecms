cf6355ec55c07a1518afbf1d71d91baaab28b533
raulir <rauli@bytecrackers.com>
2022-01-12 17:34:02 +0000

    #590 basic popup

---
+++ modules/basic/js/popup.js
@@ -0,0 +1,72 @@
+function basic_popup_init(){
+
+	// remove popup
+	$('.basic_popup_cancel').off('click.cms').on('click.cms', function(){
+		
+		var $container = $(this).closest('.basic_popup_container');
+		$container.css({'opacity':''});
+		
+		setTimeout(function(){
+			
+			$container.css({'display':'none'});
+			
+			if (typeof cms_scroll_unlock != 'undefined'){
+				cms_scroll_unlock()
+			}
+			
+			if ($container.closest('.basic_popup_wrapper').length){
+				$container.closest('.basic_popup_wrapper').remove()
+			}
+			
+		}, 300);
+
+	});
+	
+}
+
+function basic_popup_resize(){
+
+}
+
+function basic_popup_run(popup_id, wrapper_class = false){
+	
+	return new Promise ((resolve, reject) => {
+	
+		if (typeof cms_scroll_lock != 'undefined'){
+			cms_scroll_lock()
+		}
+	
+		// load popup panel
+		get_ajax_panel('basic/popup', {'popup_id': popup_id}).then((data) => {
+			
+			if (wrapper_class){
+				$(document.body).append('<span class="basic_popup_wrapper ' + wrapper_class + '">' + data.result._html + '</div>')
+			} else {
+				$(document.body).append(data.result._html)
+			}
+			
+			var $container = $('.basic_popup_' + popup_id);
+	
+			$container.css({'display':'block'});
+			setTimeout(function(){
+				$container.css({'opacity':'1'});
+			}, 50)
+			
+			resolve()
+	
+		})
+	})
+
+}
+
+$(document).ready(function() {
+	
+	$(window).on('resize.cms', function(){
+		basic_popup_resize();
+	});
+	
+	basic_popup_init();
+
+	basic_popup_resize();
+	
+});
---
+++ modules/basic/templates/popup.tpl.php
@@ -0,0 +1,7 @@
+<div class="basic_popup_container <?= !empty($popup_id) ? 'basic_popup_'.$popup_id : '' ?>">
+	<div class="basic_popup_overlay"></div>
+	<div class="basic_popup_cancel"></div>
+	<div class="basic_popup_content" <?php _ib($background, 600) ?>>
+		<!-- here comes the content -->
+	</div>
+</div>
